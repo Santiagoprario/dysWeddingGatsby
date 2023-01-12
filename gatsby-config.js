94b1fcda8c8edcde503f86305339b8f977b99fb6
Santiago Prario <82683684+Santiagoprario@users.noreply.github.com>
2023-01-12 17:12:49 -0300

    feat: :fire: initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
 /**
- * @type {import('gatsby').GatsbyConfig}
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby + Vercel`,
-    siteUrl: `https://gatsby-template.vercel.app/`,
-  },
-  plugins: [],
+  /* Your site config here */
+  pathPrefix: `/wedding`,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-eslint',
+    `gatsby-plugin-emotion`,
+    'gatsby-optional-chaining',
+    'clipboard-copy',
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        aliases: {
+          '@components': 'src/components',
+          '@helpers': 'src/helpers',
+          '@pages': 'src/pages',
+          '@assets': 'src/assets',
+          '@hooks': 'src/hooks',
+          '@': 'src',
+        },
+      },
+    },
+  ],
 };
