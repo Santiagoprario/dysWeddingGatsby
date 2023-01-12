51aa35f5f1a33d305ecbbe82fd84f2eb2da1142c
Santiago Prario <82683684+Santiagoprario@users.noreply.github.com>
2023-01-12 20:26:27 -0300

    Update index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,31 +8,17 @@ import '@assets/css/icomoon.css';
 import '@assets/css/bootstrap.css';
 import '@assets/css/style.css';
 
-const IMAGE_URL = `https://miftahussalam.com/images/khitbahmeta.jpg`;
-const META_DESCRIPTION = `Dengan memohon Rahmat dan Ridho Illahi, teriring niat menjalankan Sunnah Rasulullah ﷺ untuk membentuk rumah tangga yang Sakinah, Mawaddah wa Rahmah, kami mohon do'a agar senantiasa diberikan kelancaran dan keberkahan. - Arin & Miftah`;
-
 function MainLayout({ children }) {
   return (
     <Fragment>
       <Helmet>
-        <title>Arin ❤️ Miftah Wedding</title>
-
-        {/* Favicon */}
-        <link rel="icon" type="image/png" href={Favicon} />
-
+        <title>Dai ❤️ Santi</title>
         {/* font and SEO tags */}
-        <meta property="og:title" content="The Wedding of Arin & Miftah" />
-        <meta property="og:image" itemprop="image" content={IMAGE_URL} />
+        <meta property="og:title" content="La boda de Dai y Santi" />
         <meta property="og:url" content="https://arin.miftahussalam.com" />
-        <meta property="og:site_name" content="The Wedding of Arin & Miftah" />
-        <meta property="og:description" content={META_DESCRIPTION} />
+        <meta property="og:site_name" content="La boda de Dai y Santi" />
         <meta property="og:type" content="website" />
-        <meta name="twitter:title" content="The Wedding of Arin & Miftah" />
-        <meta name="twitter:description" content={META_DESCRIPTION} />
-        <meta name="twitter:image" content={IMAGE_URL} />
-        <meta name="twitter:url" content="https://arin.miftahussalam.com" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="@mif_salam" />
+        <meta name="twitter:title" content="La boda de Dai y Santi" />
 
         <link
           href="https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700"
